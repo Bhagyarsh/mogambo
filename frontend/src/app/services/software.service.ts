e126b2ca4656a9dc79834cf9131d25217e750bf2
Priyesh shukla <priyesh.shukla070@gmail.com>
2019-01-23 16:46:54 +0530

    401 error

--- frontend/src/app/services/software.service.ts
+++ frontend/src/app/services/software.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Token } from '@angular/compiler';
 
-// const httpOptions = {
-//   headers: new HttpHeaders({
-//     'Content-Type': 'application/json',
-//     'Authorization': 'JWT token',
-//     'Accept': 'application/json'
-//   })
-// };
+
+let _Token = localStorage.getItem('token')
+const httpOptions = {
+  headers: new HttpHeaders({
+   // 'Content-Type': 'application/json',
+    'Authorization': `JWT ${_Token}`,
+    //'Accept': 'application/json'
+  })
+};
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoftwareService {
-  _Token = localStorage.getItem('token')
-
+    token = localStorage.getItem('token'); 
+  
   private _listUrl = "http://127.0.0.1:8000/api/v1/software/"
   private _createUrl = "http://127.0.0.1:8000/api/v1/software/create"
 
-  createAuthorizationHeader(headers: HttpHeaders) {
-    headers.append('Authorization', 'JWT ' +
-      this._Token);
-  }
+  // createAuthorizationHeader(headers: HttpHeaders) {
+  //   headers.append('Authorization', 'JWT ' +
+  //     token);
+  // }
 
 
 constructor(public http: HttpClient) { }
@@ -33,8 +34,8 @@ getAll() {
 }
 
 create(softwareData) {
-  console.log(softwareData,this._Token)
-  return this.http.post<any>(this._createUrl, softwareData, { headers: new HttpHeaders })
+  console.log(softwareData,this.token)
+  return this.http.post<any>(this._createUrl, softwareData, httpOptions)
     .pipe(
       //catchError(this.handleError)
     )
