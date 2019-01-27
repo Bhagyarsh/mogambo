49513cbde75d3ea5caa7d7c6b5343297ddfd32cb
bhagyarsh dhumal <bhagyarsh@gmail.com>
2019-01-27 21:12:51 +0530

    save and then eject

--- frontend/src/app/services/software.service.ts
+++ frontend/src/app/services/software.service.ts
@@ -17,13 +17,8 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class SoftwareService {
-<<<<<<< HEAD
-    token = localStorage.getItem('token');
+   // token = localStorage.getItem('token');
 
-=======
-   // token = localStorage.getItem('token'); 
-  
->>>>>>> 4e3630cc1dd66a04cc9f25a3762bf1ac4a4216d9
   private _listUrl = "http://127.0.0.1:8000/api/v1/software/"
   private _createUrl = "http://127.0.0.1:8000/api/v1/software/create"
 
@@ -40,13 +35,7 @@ getAll() {
 }
 
 create(softwareData) {
-<<<<<<< HEAD
-  console.log(softwareData,this.token)
-
-  console.log(_Token)
-=======
   console.log(softwareData,httpOptions,_Token)
->>>>>>> 4e3630cc1dd66a04cc9f25a3762bf1ac4a4216d9
   return this.http.post<any>(this._createUrl, softwareData, httpOptions)
     .pipe(
       //catchError(this.handleError)
